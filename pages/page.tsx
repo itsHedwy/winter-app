758c8fb29aa1ba74ddcf32383801bc2337c62a03
itsHedwy <itshedwy@gmail.com>
2023-01-09 20:19:00 +0300

    Update page.tsx

--- pages/page.tsx
+++ pages/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link';
-import bg from '../images'
+
 
 export default function FirstPost() {
   return (
