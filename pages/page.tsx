fc5d88da5de095b61f73c149a3f420ca6ecd1ee2
itsHedwy <itshedwy@gmail.com>
2023-01-09 17:13:20 +0300

    Update page.tsx

--- pages/page.tsx
+++ pages/page.tsx
@@ -7,10 +7,12 @@ import Link from 'next/link';
 export default function FirstPost() {
   return (
     <>
+    <p style="background-image: url('img_girl.jpg');">
       <h1>First Post</h1>
       <h2>
         <Link href="/">Back to home</Link>
       </h2>
+    </p>
     </>
   );
 }
